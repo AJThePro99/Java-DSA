5b4f6531e414053b129a4100b10bb043c4cf6043
Aadith Jagan <aadithjonline@gmail.com>
2023-08-30 21:16:26 +0530

    Extra line removed

--- ArraysAndLists/Arrays1.java
+++ ArraysAndLists/Arrays1.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class Arrays1 {
     public static void main(String[] args) {
         int[] numberArray = new int[5];
-        numberArray[0] = 1;
 
         for (int i = 0; i < numberArray.length; i++) {
             numberArray[i] = i * i;
