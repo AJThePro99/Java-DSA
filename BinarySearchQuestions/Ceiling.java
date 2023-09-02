b7f574533a9492ab1ea3805002b48ec27e1e24eb
Aadith Jagan <aadithjonline@gmail.com>
2023-09-02 09:42:52 +0530

    Added basic binary search in ceiling()

--- BinarySearchQuestions/Ceiling.java
+++ BinarySearchQuestions/Ceiling.java
@@ -9,5 +9,31 @@ public class Ceiling {
             System.out.print("Enter number: ");
             array[i] = input.nextInt();
         }
+        System.out.print("Your target Number: ");
+        int target = input.nextInt();
+
+        ceiling(array, target);
+    }
+    static int ceiling(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (arr[mid] > target) {
+                end = mid - 1;
+            }
+            else if (arr[mid] < target) {
+                start = mid + 1;
+            }
+            else {
+                return -1;
+            }
+        }
+        return -1;
     }
 }
