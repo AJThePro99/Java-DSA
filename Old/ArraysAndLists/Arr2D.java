ce5a7e24528a4fee4701ca183fa7fd5de28d29ba
Aadith Jagan <aadithjonline@gmail.com>
2024-02-14 13:33:39 +0530

    Fixed blunder of putting dir 'Old' outside repo

---
+++ Old/ArraysAndLists/Arr2D.java
@@ -0,0 +1,10 @@
+public class Arr2D {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {2, 3, 8},
+            {5, 1, 4},
+            {9, 0, 6}
+        };
+        System.out.println(arr[1][2]);
+    }
+}
---
+++ Old/ArraysAndLists/ArrayObj.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ArrayObj {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String[] str = new String[4];
+
+        for (int i = 0; i < str.length; i++) {
+            str[i] = in.next();
+        }
+
+        System.out.println(Arrays.toString(str));
+    }    
+}
