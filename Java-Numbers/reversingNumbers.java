b134bbb9e92e699f4f2fb803d5c97fb13870388f
Aadith Jagan <aadithjonline@gmail.com>
2024-02-15 16:09:02 +0530

    Made reversingNumbers with 3 different methods

---
+++ Java-Numbers/reversingNumbers.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class reversingNumbers {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number: ");
+       int number = input.nextInt();
+
+       System.out.println(reverseUsingWhile(number));
+       System.out.println(reverseUsingFor(number));
+       System.out.println(reverseUsingRecursion(number, 0));
+    }
+
+    public static int reverseUsingWhile(int number) {
+        int ans = 0;
+        while (number > 0) {
+            ans = (ans * 10) + (number % 10);
+            number /= 10;
+        }
+        return ans;
+    }
+
+    public static int reverseUsingFor(int number) {
+        int reversed = 0;
+        for (; number > 0; number /= 10) {
+            reversed = (reversed * 10) + (number % 10);
+        }
+        return reversed;
+    }
+
+    public static int reverseUsingRecursion(int number, int reversed) {
+
+        if (number > 0) {
+            reversed = (reversed * 10) + number % 10;
+            number /= 10;
+            return reverseUsingRecursion(number, reversed);            
+        }
+        else {
+            return reversed;
+        }
+    }
+}
