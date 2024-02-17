a6b199dbe4fdea8f1f44e5c078dc6802302789d8
Aadith Jagan <aadithjonline@gmail.com>
2024-02-17 20:48:39 +0530

    Suppresed warnings of input functions for cleaner experince of VSCode

---
+++ Java-Numbers/numberToWords.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class numberToWords {
+    public static void main(String[] args) {
+        // if input is 54,297 then the output should be:  Fifty-four thousand two hundred ninety-seven.
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int number = input.nextInt();
+
+        
+
+    }
+
+    public static String numberToString() {
+        
+
+        return "";
+    }
+
+}
--- Java-Numbers/reversingNumbers.java
+++ Java-Numbers/reversingNumbers.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 class reversingNumbers {
     public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
+       @SuppressWarnings("resource")
+    Scanner input = new Scanner(System.in);
        System.out.print("Enter number: ");
        int number = input.nextInt();
 
